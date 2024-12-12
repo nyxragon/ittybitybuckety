624c83817c4e649118f3eceda940418743e84172
abhi-ingle <abhijeet171202@gmail.com>
2024-12-12 18:45:35 +0530

    logic changes

--- cli/main.go
+++ cli/main.go
@@ -4,15 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
-	"bitybuckety"
+	"github.com/nyxragon/bitybuckety"
 )
 
 func main() {
 	// Parse CLI flags
 	totalCommits := flag.Int("total", 100, "Total number of commits to fetch")
-	date := flag.String("date", "2024-01-01T00:00:00+00:00", "Fetch commits before this date")
+	date := flag.String("date", "2024-12-12T00:00:00+00:00", "Fetch commits before this date")
 	flag.Parse()
+
+	// Check if the totalCommits value is valid
+	if *totalCommits <= 0 {
+		fmt.Println("Error: 'total' must be greater than 0.")
+		os.Exit(1)
+	}
 
 	// Fetch commits and write to file
 	filename, err := bitybuckety.FetchCommitsAndWriteFile(*totalCommits, *date)
@@ -20,5 +27,6 @@ func main() {
 		log.Fatalf("Error: %v\n", err)
 	}
 
+	// Output the success message with the file where commits are stored
 	fmt.Printf("Commits successfully written to %s\n", filename)
 }
